6a9d7eb2733848c834a7c7c80f13ff3208812f30
Colin Van Delden <90783309+cmvandel@users.noreply.github.com>
2022-10-11 21:29:16 -0400

    Rowy Racers display text
    now game displays if you have won or lost
    
    Also displays which racer won

--- Blackstar Carnival/Assets/Scripts/RowdyRacersGame.cs
+++ Blackstar Carnival/Assets/Scripts/RowdyRacersGame.cs
@@ -2,48 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class RowdyRacersGame : MonoBehaviour
 {
 
-    public GameObject panel;
+    public GameObject betPanel;
+    public GameObject resultPanel;
+    public GameObject result;
+    public TextMeshProUGUI resultText;
 
     public void Start()
     {
-        panel = GameObject.Find("BettingPanel");
-        panel.SetActive(true);
+        betPanel = GameObject.Find("BettingPanel");
+        betPanel.SetActive(true);
+        resultPanel = GameObject.Find("Result Panel");
+        resultPanel.SetActive(false);
+        result = GameObject.Find("Result Panel/Result");
+        resultText = result.GetComponent<TextMeshProUGUI>();
     }
-
 
     public void bet(int racer)
     {
+        //decide winner
         int winner = generateWinner();
-        
-        StartCoroutine(raceTime());
 
-        if(racer == winner)
+
+        //wait x seconds
+        StartCoroutine(raceTime(5));
+
+        //play race animation in the meantime
+        
+        //display results
+        resultPanel.SetActive(true);
+
+        if (racer == winner)
         {
-            Debug.Log("You win");
+            resultText.text = $"You Win!";
         }
         else
         {
-            Debug.Log("You lose");
+            resultText.text = $"You Lost!\n Racer {winner} has won";
         }
+
     }
 
     public int generateWinner()
     {
         int winner = Random.Range(1, 5);
-        Debug.Log("Winner is" + winner);
+        Debug.Log("Winner is " + winner);
         return winner;
     }
 
-    IEnumerator raceTime()
+    IEnumerator raceTime(int time)
     {
-        panel.SetActive(false);
-        yield return new WaitForSeconds(5);
-        panel.SetActive(true);
+        betPanel.SetActive(false);
+        yield return new WaitForSeconds(time);
     }
 
+    public void playAgain()
+    {
+        betPanel.SetActive(true);
+        resultPanel.SetActive(false);
+    }
 
 }
