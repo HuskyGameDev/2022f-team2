17e563145edbc04f84365b5e47aeb9b8f983e800
Colin Van Delden <90783309+cmvandel@users.noreply.github.com>
2023-03-23 19:08:09 -0400

    DD lvl3 and lvl4
    level 3 and level4 for drum duelist complete

--- Blackstar Carnival/Assets/Scripts/Games/Drum Duelist/beat.cs
+++ Blackstar Carnival/Assets/Scripts/Games/Drum Duelist/beat.cs
@@ -6,8 +6,11 @@ public class beat : MonoBehaviour
 {
     public GameObject track;
     public GameObject mainCanvas;
+    public GameObject levelSelect;
     private RectTransform trackTransform;
     private RectTransform rectTransform;
+    private Dictionary<string, object> levelInfo;
+
     float tempo; 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +19,13 @@ public class beat : MonoBehaviour
         rectTransform = this.GetComponent<RectTransform>();
         tempo = trackTransform.rect.height / 15000;
         rectTransform.anchoredPosition = new Vector3(0, 0, 0);
+        levelInfo = levelSelect.GetComponent<levelSelect>().levelInfo;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rectTransform.Translate(0, tempo * -1, 0, mainCanvas.transform);
+        rectTransform.Translate(0, tempo * -1 * (float)levelInfo["tempoFactor"], 0, mainCanvas.transform);
     }
 
 }
