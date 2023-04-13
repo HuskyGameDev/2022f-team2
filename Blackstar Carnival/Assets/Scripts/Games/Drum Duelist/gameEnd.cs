823dd057a07871934316abc2fa29089a7c9497fa
Colin Van Delden <90783309+cmvandel@users.noreply.github.com>
2023-04-13 19:20:05 -0400

    bug fixes
    bug fixes on dd end game screen

--- Blackstar Carnival/Assets/Scripts/Games/Drum Duelist/gameEnd.cs
+++ Blackstar Carnival/Assets/Scripts/Games/Drum Duelist/gameEnd.cs
@@ -14,23 +14,26 @@ public class gameEnd : MonoBehaviour
     // Start is called before the first frame update
     void OnEnable()
     {
-        finalScoreText.text = "You Lose!\nYour Final Score is: " + gameManager.score;
-
-        if (gameManager.score > 4000 && gameManager.tempoFactor == 0.6f)
+        if(gameManager.score <= 4000)
+        {
+            finalScoreText.text = "You Lose!\nYour Final Score is: " + gameManager.score;
+        }
+        if (gameManager.score <= 8000)
         {
             StarBucksManager.Instance.UpdateBucks(1);
             finalScoreText.text = "Congradulations!\nFinal Score: " + gameManager.score + "\nStarbucks earned: 1";
         }
-        else if(gameManager.score > 6000 && gameManager.tempoFactor == 0.8f)
+        else if(gameManager.score <= 12000)
         {
             StarBucksManager.Instance.UpdateBucks(2);
             finalScoreText.text = "Congradulations!\nFinal Score: " + gameManager.score + "\nStarbucks earned: 2";
         }
-        else if(gameManager.score > 12000 && gameManager.tempoFactor == 1.15f)
+        else
         {
             StarBucksManager.Instance.UpdateBucks(3);
             finalScoreText.text = "Congradulations!\nFinal Score: " + gameManager.score + "\nStarbucks earned: 3";
         }
+
     }
 
     public void playAgain()
